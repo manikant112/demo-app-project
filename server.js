4e090dd61eb2aa528a5a0b7b97341f40e0270ad4
manikant112 <153704846+manikant112@users.noreply.github.com>
2023-12-15 17:13:58 +0530

    added v2 testing

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/api/v1/test', (request, response, next) => {
     response.send({ success: false });
 });
-
+app.get('/api/v2/test',(req,res) => {
+    res.send({message : "v2 version is working fine")
+}
 server.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
 });
